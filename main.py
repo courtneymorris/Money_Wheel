fc38e60ca4dcb4febe8e10faae30aa392a18268c
Courtney Morris <shull.courtney@gmail.com>
2021-08-06 10:04:22 -0600

    Added functionality to prize wheel

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-# Prizes
+import random
 
-wheel = {
-    '0': 18,
-    '$10': 4,
-    '$50': 3,
+
+prize_wheel = {
+    '0': 12,
+    '$5': 6,
+    '$25': 3,
     '$100': 2,
     '$500': 1
 }
+
+def money_wheel(prizes):
+    segments = []
+
+    for prize, chance in prizes.items():
+        for _ in range(chance):
+            segments.append(prize)
+
+    return random.choice(segments)
+
+
+print(money_wheel(prize_wheel))
