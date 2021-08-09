6b32e7838253161b9a6cb6681cf2dbb0a890742f
Courtney Morris <shull.courtney@gmail.com>
2021-08-08 23:31:48 -0600

    Worked on wallet functionality. Got frustrated.

--- main.py
+++ main.py
@@ -11,23 +11,42 @@ def add_profile():
 
 
 
-prize_wheel = {
-    '0': 6,
-    '1': 5,
-    '$5': 4,
-    '$25': 3,
-    '$100': 2,
-    '$500': 1
+
+
+money_wheel = {
+    0 : 6,
+    1: 5,
+    5 : 4,
+    25 : 3,
+    100 : 2,
+    500 : 1
 }
 
-def money_wheel(prizes):
-    segments = []
+my_wallet = 15
 
-    for prize, chance in prizes.items():
+def spin(wheel, wallet):
+    segments = []
+    
+    for prize, chance in wheel.items():
         for _ in range(chance):
             segments.append(prize)
+            prize_won = random.choice(segments)
+    wallet = wallet + (prize_won - 5)
 
-    return random.choice(segments)
+    if prize_won != 0:
+        print(f'Congratulations! You\'ve won ${prize_won}! You have ${wallet} left. Let\'s play again!')
+    else:
+        print(f'Oof, bad luck! Want to spin again?')
+
+    return wallet
+
+
+my_wallet = spin(money_wheel, my_wallet)
+my_wallet = spin(money_wheel, my_wallet)
+my_wallet = spin(money_wheel, my_wallet)
+my_wallet = spin(money_wheel, my_wallet)
+my_wallet = spin(money_wheel, my_wallet)
+my_wallet = spin(money_wheel, my_wallet)
 
 
 
